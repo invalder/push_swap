6c82f34c49252552bb9033911345137dc3eb5a3c
Nuttapon N <ponidc@gmail.com>
2022-05-09 14:11:53 +0700

    modify input error message

--- srcs/push_swap_err_msg.c
+++ srcs/push_swap_err_msg.c
@@ -6,7 +6,7 @@
 /*   By: nnakarac <nnakarac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 14:06:29 by nnakarac          #+#    #+#             */
-/*   Updated: 2022/05/08 15:31:27 by nnakarac         ###   ########.fr       */
+/*   Updated: 2022/05/09 14:10:31 by nnakarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 #define ARGREQ1 "./push_swap <args-1> .... <args-n>\n"
 #define ARGREQ2 "./push_swap \"args-1 args-2 .... args-n\"\n"
+#define ARGREQ3 "ARG=\"args-1 args-2 .... args-n\"; ./push_swap $ARG\n"
 
 void	ft_exit_req_args(void)
 {
-	ft_printf("<<TRY THIS>>\n%sor\n%s", ARGREQ1, ARGREQ2);
+	ft_printf("<<TRY THIS>>\nARG:\n%sor\n1 ARG:\n%sor\nSTDIN:\n%s<<TRY THIS>>\n", ARGREQ1, ARGREQ2, ARGREQ3);
 	exit(1);
 }
 
